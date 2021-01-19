987e8957ef02c1119de1fa70715341790331f202
DogSobre <juanfernandoherrada13@gmail.com>
2021-01-19 15:55:04 +0100

    add inception5h file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[]agrv){
         System.out.println("Hello Wolrd");
+        BufferedImage img = null;
+
+        try
+        {
+            img = ImageIO.read(new File("C:/ImageTest/pic2.jpg")); // eventually C:\\ImageTest\\pic2.jpg
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
