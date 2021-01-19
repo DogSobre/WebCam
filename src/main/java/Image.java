ece2f8223b8e82c627232e5cebe739b811095403
Francesco <fha@m2mfactory.fr>
2021-01-19 16:14:56 +0100

    maj

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Image {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+
+public class Image {
+
+
+    private String filePath;
+
+    public Image(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public byte[] imageToByte() throws IOException {
+        // open image
+        File imgPath = new File(this.getFilePath());
+        BufferedImage bufferedImage = ImageIO.read(imgPath);
+
+        // get DataBufferBytes from Raster
+        WritableRaster raster = bufferedImage.getRaster();
+        DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+
+        return (data.getData());
+
+    }
 }
