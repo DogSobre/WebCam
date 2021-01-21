16f3a5ec9f44816752dcc161eb1a9b73dea8e2d3
DogSobre <juanfernandoherrada13@gmail.com>
2021-01-21 12:21:08 +0100

    Filter v1(en brute pour une image)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] agrv) throws Exception {
         Application.launch(ImagesApp.class, agrv);
+        Application.launch(Filters.class, agrv);
     }
 
 }
